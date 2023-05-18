56f00f7c2c0775a2b22b21cdb73efe5415611e37
mickaellebihan <lebihan.mickael@gmail.com>
2023-05-18 15:16:04 +0200

    Added Dtos and static Roles

---
+++ Dto/LoginDto.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace back.Dto
+{
+	public class LoginDto
+	{
+		[Required(ErrorMessage = "Le nom d'utilisateur est requis")]
+		public string UserName { get; set; }
+
+		[Required(ErrorMessage = "Le mot de passe est requis")]
+		public string Password { get; set; }
+
+	}
+}
---
+++ Dto/OtherObjects.cs
@@ -0,0 +1,10 @@
+﻿namespace back.Dto
+{
+	public static class OtherObjects
+	{
+		public const string ADMIN = "ADMIN";
+		public const string PRODUCER = "PRODUCER";
+		public const string COMMUNITY_MANAGER = "COMMUNITY_MANAGER";
+		public const string USER = "USER";
+	}
+}
