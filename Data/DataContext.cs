8c63e58f666c5e7a90b92946e9a247080192f5ac
mickaellebihan <lebihan.mickael@gmail.com>
2023-05-18 15:04:40 +0200

    base implementation of identity

--- Data/DataContext.cs
+++ Data/DataContext.cs
@@ -1,11 +1,12 @@
 ﻿using back.Models;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
 
 namespace back.data
 {
-	public class DataContext : DbContext
+	public class DataContext : IdentityDbContext
 	{
-		public DataContext(DbContextOptions options) : base(options) {
+		public DataContext(DbContextOptions<DataContext> options) : base(options) {
 		}
 
 		public DbSet<Game> Games { get; set; }
